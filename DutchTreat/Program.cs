b919e018f660b1bf4906bbea3bc9c1a4637086e1
melodie <josmelodie@gmail.com>
2018-06-02 17:17:00 +1200

    Add project files.

---
+++ DutchTreat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run(); //listening for requests
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>() //how to listen to web requests
+                .Build();
+    }
+}
